cb8aeb3f02f3bd541943dd19c79e80148229ba52
Showrab <supto50showrab@gmail.com>
2025-10-27 04:09:17 +0600

    new page

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 // services/api.js
 import axios from "axios";
 
-const API_URL = "https://cricket-league-backend.vercel.app"; // Change for production
+const API_URL = "http://localhost:5000"; // Change for production
 
 // Create axios instance
 const api = axios.create({
