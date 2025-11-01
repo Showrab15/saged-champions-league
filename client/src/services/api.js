5f35d3bdb06933f0877ab52e636a11386dc1049f
Showrab <supto50showrab@gmail.com>
2025-11-01 14:38:19 +0600

    api uel changed

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL; // ✅ Loaded from environment variable
+const API_URL = "https://cricket-league-backend.vercel.app"; // ✅ Loaded from environment variable
 
 // Create axios instance
 const api = axios.create({
